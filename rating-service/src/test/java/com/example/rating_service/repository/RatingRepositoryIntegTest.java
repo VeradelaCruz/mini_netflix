8c37ed4f07de2e8484cab9ea9878ef2c069e5964
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-08-27 08:40:53 +0200

    test: add integration tests for RatingRepository.findAll

---
+++ rating-service/src/test/java/com/example/rating_service/repository/RatingRepositoryIntegTest.java
@@ -0,0 +1,88 @@
+package com.example.rating_service.repository;
+
+import com.example.rating_service.enums.Score;
+import com.example.rating_service.mapper.RatingMapper;
+import com.example.rating_service.models.Rating;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(properties = {
+        "spring.cloud.config.enabled=false",
+        "eureka.client.enabled=false"
+})
+@ActiveProfiles("test")
+public class RatingRepositoryIntegTest {
+    @Autowired
+    private RatingRepository ratingRepository;
+
+    @Autowired
+    private RatingMapper ratingMapper;
+
+    private Rating rating1;
+    private Rating rating2;
+    private Rating rating3;
+    private List<Rating> ratingList;
+
+
+    @BeforeEach
+    void  setUp(){
+        ratingRepository.deleteAll();
+
+        rating1= new Rating();
+        rating1.setId("1L");
+        rating1.setUserId("U1");
+        rating1.setMovieId("C1");
+        rating1.setScore(Score.THREE_STARS);
+        rating1.setComment("----");
+        rating1.setCreatedAt(LocalDate.now());
+
+        rating2= new Rating();
+        rating2.setId("2L");
+        rating2.setUserId("U2");
+        rating2.setMovieId("C2");
+        rating2.setScore(Score.FOUR_STARS);
+        rating2.setComment("----");
+        rating2.setCreatedAt(LocalDate.now());
+
+        rating3= new Rating();
+        rating3.setId("3L");
+        rating3.setUserId("U3");
+        rating3.setMovieId("C3");
+        rating3.setScore(Score.FIVE_STARS);
+        rating3.setComment("----");
+        rating3.setCreatedAt(LocalDate.now());
+
+        ratingList= List.of(rating1, rating2, rating3);
+    }
+
+    @Test
+    @DisplayName("Should save a list with ratings")
+    void saveAll_ShouldReturnAList(){
+        ratingRepository.saveAll(ratingList);
+
+        List<Rating> ratings= ratingRepository.findAll();
+        assertNotNull(ratings);
+
+        assertEquals(3, ratings.size());
+    }
+
+    @Test
+    @DisplayName("Should find all ratings")
+    void findAll_ShouldReturnAList(){
+        ratingRepository.saveAll(ratingList);
+
+        List<Rating> list=ratingRepository.findAll();
+        assertEquals(3, list.size());
+
+    }
+}
