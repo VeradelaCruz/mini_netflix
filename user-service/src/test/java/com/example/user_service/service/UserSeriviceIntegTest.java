bf63ad7b9d68a58278a6aa48f86db536dc8a8864
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-09-02 17:56:57 +0200

    test: add integration test for creating and saving users

--- user-service/src/test/java/com/example/user_service/service/UserSeriviceIntegTest.java
+++ user-service/src/test/java/com/example/user_service/service/UserSeriviceIntegTest.java
@@ -1,12 +1,32 @@
 package com.example.user_service.service;
 
+import com.example.user_service.client.CatalogClient;
+import com.example.user_service.client.RatingClient;
 import com.example.user_service.config.CacheTestConfig;
 import com.example.user_service.config.MongoTestConfig;
 import com.example.user_service.config.RedisConfig;
+import com.example.user_service.dtos.RatingScoreDTO;
+import com.example.user_service.dtos.RatingUserDTO;
+import com.example.user_service.dtos.UserDTO;
+import com.example.user_service.dtos.UserRoleDTO;
+import com.example.user_service.enums.Role;
+import com.example.user_service.mapper.UserMapper;
+import com.example.user_service.models.User;
+import com.example.user_service.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(properties = {
         "spring.cloud.config.enabled=false",
@@ -22,5 +42,71 @@ import org.springframework.test.context.ActiveProfiles;
 // Esto evita que Spring intente cargar Redis y su CacheManager real, que no necesitamos en tests
 @ImportAutoConfiguration(exclude = RedisConfig.class)
 public class UserSeriviceIntegTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private RatingClient ratingClient;
+
+    @MockBean
+    private CatalogClient catalogClient;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    private User user1;
+    private User user2;
+    private User user3;
+    List<User> userList;
+
+    //DTOs
+    private UserDTO userDTO;
+    private RatingScoreDTO ratingScoreDTO;
+    private RatingUserDTO ratingUserDTO;
+    private UserRoleDTO roleDTO;
+
+    @BeforeEach
+    void setUp(){
+        userRepository.deleteAll();
+
+        user1= new User();
+        user1.setUserId("1L");
+        user1.setEmail("email1@gmail.com");
+        user1.setPreferences(List.of("ACTION", "SCI_FIC"));
+        user1.setRole(Role.USER);
+
+        user2= new User();
+        user2.setUserId("2L");
+        user2.setEmail("email2@gmail.com");
+        user2.setPreferences(List.of("DRAMA", "SCI_FIC"));
+        user2.setRole(Role.USER);
+
+        user3= new User();
+        user3.setUserId("3L");
+        user3.setEmail("email3@gmail.com");
+        user3.setPreferences(List.of("ACTION", "DRAMA"));
+        user3.setRole(Role.USER);
+
+        userList = List.of(user1, user2, user3);
+    }
+
+    @Test
+    @DisplayName("Should crate an user if it's not empty")
+    void createUser_shouldReturnAUserList(){
+        List<User> list= userService.createUser(userList);
+
+        assertNotNull(list);
+        assertEquals(3, list.size());
+
+        List<User> result= userRepository.saveAll(userList);
+        assertNotNull(result);
+        assertEquals(3, result.size());
+
+    }
+
 
 }
