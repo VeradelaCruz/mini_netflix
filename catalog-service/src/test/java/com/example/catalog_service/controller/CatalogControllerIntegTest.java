9ab5db46cff9f00976a9ee821ed54abc9a6e8d9f
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-09-04 09:59:35 +0200

    fix: configuration for redis in CatalogControllerIntegTest

--- catalog-service/src/test/java/com/example/catalog_service/controller/CatalogControllerIntegTest.java
+++ catalog-service/src/test/java/com/example/catalog_service/controller/CatalogControllerIntegTest.java
@@ -1,5 +1,8 @@
 package com.example.catalog_service.controller;
 
+import com.example.catalog_service.config.CacheTestConfig;
+import com.example.catalog_service.config.MongoTestConfig;
+import com.example.catalog_service.config.RedisConfig;
 import com.example.catalog_service.dtos.CatalogUpdateDto;
 import com.example.catalog_service.dtos.RatingScoreDTO;
 import com.example.catalog_service.enums.Genre;
@@ -10,6 +13,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -28,8 +33,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(properties = {
         "spring.cloud.config.enabled=false",
         "eureka.client.enabled=false"
+
 })
 @ActiveProfiles("test")
+// Indicamos a Spring que importe estas configuraciones adicionales solo para este test
+// Esto permite usar beans definidos en MongoTestConfig y CacheTestConfig
+@Import({MongoTestConfig.class, CacheTestConfig.class})
+
+// Excluimos la configuración automática de Redis para este test
+// Esto evita que Spring intente cargar Redis y su CacheManager real, que no necesitamos en tests
+@ImportAutoConfiguration(exclude = RedisConfig.class)
 @AutoConfigureMockMvc
 public class CatalogControllerIntegTest {
     @Autowired
